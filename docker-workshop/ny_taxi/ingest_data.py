a14cadd0e3fd6227342d23024df14cfbb4b3dc6a
Marcin Sola <marcin.sola@profitroom.com>
2026-01-03 17:53:29 +0100

    Pipeline as a CLI tool

--- docker-workshop/ny_taxi/ingest_data.py
+++ docker-workshop/ny_taxi/ingest_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
@@ -29,18 +30,17 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-def run():
-    pg_username = "root"
-    pg_password = "root"
-    pg_host = "localhost"
-    pg_port = 5432
-    pg_database = "ny_taxi"
-
-    year = 2021
-    month = 1
-
-    chunksize = 100000
-    target_table = "yellow_taxi_data"
+@click.command()
+@click.option('--year', default=2021, type=int, help='Year for the taxi data')
+@click.option('--month', default=1, type=int, help='Month for the taxi data')
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--chunksize', default=100000, type=int, help='Number of rows to process in each chunk')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+def main(year, month, pg_user, pg_pass, pg_host, pg_port, pg_db, chunksize, target_table):
 
     csv_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_{year}-{month:02d}.csv.gz"
     df_iter = pd.read_csv(
@@ -52,7 +52,7 @@ def run():
     )
 
 
-    engine = create_engine(f'postgresql://{pg_username}:{pg_password}@{pg_host}:{pg_port}/{pg_database}')
+    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
     first = True
     for df in tqdm(df_iter):
@@ -63,4 +63,4 @@ def run():
         df.to_sql(name=target_table, con=engine, if_exists='append')
 
 if __name__ == "__main__":
-    run()
+    main()
