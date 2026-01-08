347b22d7ece44d443d246296794280cd3c77ce02
Marcin Sola <marcin.sola@profitroom.com>
2026-01-08 19:18:59 +0100

    Add variables

--- terraform/terrademo/main.tf
+++ terraform/terrademo/main.tf
@@ -8,14 +8,15 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("creds.json")
-  project     = "de-zoomcamp-467414"
+  credentials = file(var.credentials)
+  project     = var.project
   region      = "us-central1"
 }
 
 resource "google_storage_bucket" "demo-bucket" {
-  name          = "de-zoomcamp-467414-demo-bucket"
-  location      = "US"
+  name          = var.gcs_bucket_name
+  storage_class = var.gcs_storage_class
+  location      = var.location
   force_destroy = true
 
   lifecycle_rule {
@@ -26,4 +27,9 @@ resource "google_storage_bucket" "demo-bucket" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
 }
